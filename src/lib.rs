b5ebd0e345a6f9afd9bca258826fcbc77212c97c
AlekMichael123 <alekmichael2345@gmail.com>
2024-09-16 01:54:26 -0700

    Added benchmark

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,19 @@
+use std::time::Instant;
+
 pub mod aoc;
 
 pub fn use_day(year: u16, day: u8, data: &str) -> Result<(), &str> {
   let solutions = aoc::get_year_solutions();
   let Some(solutions) = solutions.get(&year) else { 
     return Err("Year does not exist or is not implemented yet.");
-   };
+  };
   match solutions.get(&day) {
-    Some(solution) => Ok(solution(data)),
+    Some(solution) => {
+      let start = Instant::now();
+      solution(data);
+      println!("Total time elapsed: {:.2?}", start.elapsed());
+      Ok(())
+    },
     None => Err("Day does not exist or is not implemented yet."),
   }
 }
