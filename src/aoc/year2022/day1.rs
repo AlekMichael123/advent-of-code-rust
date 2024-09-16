efe53ac11d4fe8e99ceae6a812bb7b24ab271d04
AlekMichael123 <alekmichael2345@gmail.com>
2024-09-16 01:45:04 -0700

    2022 day 1 part 1 and 2 complete!  I think this is gonna be fun! <sarcasm?>

--- src/aoc/year2022/day1.rs
+++ src/aoc/year2022/day1.rs
@@ -1,27 +1,31 @@
 pub fn main(data: &str) {
+  let mut calories = parse_data(data);
+
   println!("Part 1");
-  let calories = parse_data(data);
-  let max = calories.iter().max().expect("Max error");
+  
+  let max = part1(&calories);
   println!("The elf holding the most is holding: {max}");
+  
   println!("Part 2");
+  
+  let sum_of_highest_three = part2(&mut calories);
+  println!("The sum of the highest three is: {sum_of_highest_three}"); 
 }
 
-fn part1() {}
+fn part1(calories: &Vec<u32>) -> &u32 {
+  calories.iter().max().unwrap()
+}
+
+fn part2(calories: &mut Vec<u32>) -> u32 {
+  calories.sort();
+  calories.reverse();
+  let first_three = &calories[0..3];
+  first_three.iter().sum()
+}
 
 fn parse_data(data: &str) -> Vec<u32> {
-  let mut acc: u32 = 0;
-  let mut calories: Vec<u32> = vec![];
-
   data
-    .lines()
-    .for_each(|line| {
-      if line == "" {
-        calories.push(acc);
-        acc = 0;
-      } else {
-        acc += line.parse::<u32>().expect("Parsing error");
-      }
-    });
-
-  calories
+    .split("\n\n")
+    .map(|elf| elf.lines().map(|cal| cal.parse::<u32>().unwrap()).sum::<u32>())
+    .collect()
 }
