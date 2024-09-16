9eb49ba4de7f8db5d5bdd86aa6ace9796b561c28
AlekMichael123 <alekmichael2345@gmail.com>
2024-09-16 01:32:26 -0700

    year 2022 day 1 part 1 done, refactoring

--- src/aoc/year2022/day1.rs
+++ src/aoc/year2022/day1.rs
@@ -1,3 +1,27 @@
 pub fn main(data: &str) {
-  println!("{data}");
+  println!("Part 1");
+  let calories = parse_data(data);
+  let max = calories.iter().max().expect("Max error");
+  println!("The elf holding the most is holding: {max}");
+  println!("Part 2");
+}
+
+fn part1() {}
+
+fn parse_data(data: &str) -> Vec<u32> {
+  let mut acc: u32 = 0;
+  let mut calories: Vec<u32> = vec![];
+
+  data
+    .lines()
+    .for_each(|line| {
+      if line == "" {
+        calories.push(acc);
+        acc = 0;
+      } else {
+        acc += line.parse::<u32>().expect("Parsing error");
+      }
+    });
+
+  calories
 }
