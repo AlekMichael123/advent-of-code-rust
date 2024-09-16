33fdfd3ea473631e6857eb28b4e31f5b5a9a79b5
AlekMichael123 <alekmichael2345@gmail.com>
2024-09-16 00:48:54 -0700

    input set up

--- src/aoc/year2022/mod.rs
+++ src/aoc/year2022/mod.rs
@@ -3,10 +3,9 @@ use std::collections::HashMap;
 pub mod day1;
 
 pub fn get_solutions() -> HashMap<u8, fn(&str)> {
-  let solutions: [(u8, fn(&str)); 1] = [
+  let solutions: Vec<(u8, fn(&str))> = vec![
     (1, day1::main),
   ];
 
-  // Use the `into_iter` method to consume the array and collect directly
   solutions.into_iter().collect()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
+use std::fs::read_to_string;
+
 use advent_of_code_rust::use_day;
 
 fn main() {
-    println!("Hello, world!");
-    match use_day(2022, 1, "I LOVE RUSTTTT!!! ... PFFTT!! NOTTTTTT!!!") {
-        Ok(_) => (),
-        Err(err) => println!("ERROR: {err}"),
-    }
+  match read_input_file() {
+    Ok(data) => match use_day(2022, 1, &data) {
+      Ok(_) => (),
+      Err(err) => println!("ERROR: {err}"),
+    },
+    Err(err) => println!("ERROR: {err}"),
+  }  
+}
+
+fn read_input_file() -> Result<String, String> {
+  match read_to_string("input.txt") {
+    Ok(data) => Ok(data),
+    Err(_) => Err("Could not find input file.".to_string()),
+  }
 }
