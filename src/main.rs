d3a80bf1c6639f519ee91f44acaa6ddd3ecf998c
AlekMichael123 <alekmichael2345@gmail.com>
2024-09-16 01:13:52 -0700

    cli implemented to select year and day

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use std::fs::read_to_string;
+use std::env;
 
 use advent_of_code_rust::use_day;
 
 fn main() {
+  let Ok((year, day)) = get_args() else {
+    panic!("Error: get_args failed!")
+  };
   match read_input_file() {
-    Ok(data) => match use_day(2022, 1, &data) {
+    Ok(data) => match use_day(year, day, &data) {
       Ok(_) => (),
       Err(err) => println!("ERROR: {err}"),
     },
@@ -17,4 +21,18 @@ fn read_input_file() -> Result<String, String> {
     Ok(data) => Ok(data),
     Err(_) => Err("Could not find input file.".to_string()),
   }
+}
+
+fn get_args() -> Result<(u16, u8), ()> {
+  let args: Vec<String> = env::args().collect();
+  let mut args = args.iter();
+  let (Some(_), Some(year), Some(day), None) = (args.next(), args.next(), args.next(), args.next()) else {
+    println!("Not enough arguments passed.");
+    return Err(());
+  };
+  let (Ok(year), Ok(day)) = ((*year).parse::<u16>(), (*day).parse::<u8>()) else {
+    println!("Parsing error occured");
+    return Err(());
+  };
+  Ok((year, day))
 }
