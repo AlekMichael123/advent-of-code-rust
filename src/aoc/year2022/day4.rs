8365116af47a6022b9ae027bf2a7b26ed4c3544e
AlekMichael123 <alekmichael2345@gmail.com>
2024-09-18 01:14:56 -0700

    2022 day 4 part 1 & 2 complete

--- src/aoc/year2022/day4.rs
+++ src/aoc/year2022/day4.rs
@@ -1,7 +1,23 @@
 use std::num::ParseIntError;
 
 pub fn main(data: &str) {
+  let ranges = parse_input(data);
+  println!("Part 1 -- count number of pairs which one range completely overlaps -- {}", part1(&ranges));
+  println!("Part 2 -- count number of pairs which one range partially overlaps -- {}", part2(&ranges));
+}
 
+fn part1(ranges: &Vec<(IntRange, IntRange)>) -> usize {
+  ranges
+    .iter()
+    .filter(|(a, b)| a.completely_overlaps(b) || b.completely_overlaps(a))
+    .count()
+}
+
+fn part2(ranges: &Vec<(IntRange, IntRange)>) -> usize {
+  ranges
+    .iter()
+    .filter(|(a, b)| a.partially_overlaps(b) || b.partially_overlaps(a))
+    .count()
 }
 
 fn parse_input(data: &str) -> Vec<(IntRange, IntRange)> {
@@ -31,11 +47,22 @@ impl From<ParseIntError> for IntRangeError {
   }
 }
 
+#[derive(Debug)]
 struct IntRange {
   first: u8,
   last: u8,
 }
 
+impl IntRange {
+  fn completely_overlaps(&self, range: &IntRange) -> bool {
+    self.first <= range.first && self.last >= range.last
+  }
+
+  fn partially_overlaps(&self, range: &IntRange) -> bool {
+    self.first <= range.first && self.last >= range.first 
+    || self.first <= range.last && self.last >= range.last
+  }
+}
 
 impl TryFrom<&str> for IntRange {
   type Error = IntRangeError;
