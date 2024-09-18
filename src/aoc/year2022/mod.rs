1277576b81b0248d34ac2c5c67ab19b6f856fc7d
AlekMichael123 <alekmichael2345@gmail.com>
2024-09-17 23:59:50 -0700

    2022 day 4 boilerplate

--- src/aoc/year2022/mod.rs
+++ src/aoc/year2022/mod.rs
@@ -3,12 +3,14 @@ use std::collections::HashMap;
 pub mod day1;
 pub mod day2;
 pub mod day3;
+pub mod day4;
 
 pub fn get_solutions() -> HashMap<u8, fn(&str)> {
   let solutions: Vec<(u8, fn(&str))> = vec![
     (1, day1::main),
     (2, day2::main),
     (3, day3::main),
+    (4, day4::main),
   ];
 
   solutions.into_iter().collect()
