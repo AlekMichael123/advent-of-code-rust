6a36b93e3fddd3e674a26399015d1275de328d9d
AlekMichael123 <alekmichael2345@gmail.com>
2024-09-22 02:41:32 -0700

    2022 day 8 part 1 complete, not happy w my solution

--- src/aoc/year2022/day8.rs
+++ src/aoc/year2022/day8.rs
@@ -1,1 +1,104 @@
-pub fn main(data: &str) {}
+pub fn main(data: &str) {
+  let tree_heights = parse_input(data);
+
+  println!(
+    "Part 1 -- Find number of trees visible from standing around the grid\n# of visible trees = {}",
+    part1(&tree_heights),
+  );
+}
+
+fn part1(tree_heights: &TreeHeights) -> u32 {
+  TreeVisibility::calc_tree_visibilities(tree_heights)
+    .iter()
+    .map(|row| 
+      row
+        .iter()
+        .map(|visibility| 
+          match visibility {
+            TreeVisibility::Visible => 1,
+            TreeVisibility::NotVisible => 0,
+          }
+        )
+        .sum::<u32>()
+    )
+    .sum()
+}
+
+fn parse_input(data: &str) -> TreeHeights {
+  data
+    .lines()
+    .map(|line| 
+      line
+        .chars()
+        .map(|c| c.to_digit(10).expect("parse_input: Cannot convert digit in input.") as u8)
+        .collect()
+    )
+    .collect()
+}
+
+type TreeHeights = Vec<Vec<u8>>;
+type TreeVisibilities = Vec<Vec<TreeVisibility>>;
+
+#[derive(Debug, Clone)]
+enum TreeVisibility {
+  Visible,
+  NotVisible,
+}
+
+impl TreeVisibility {
+  fn calc_tree_visibilities(tree_heights: &TreeHeights) -> TreeVisibilities {
+    let n = tree_heights.len();
+    let m = tree_heights.first().expect("calc_tree_visibilities: tree_heights contains empty vectors").len();
+    
+    let mut tree_visibilities: TreeVisibilities = vec![vec![TreeVisibility::NotVisible; m]; n];
+    
+    // mark always visible
+    tree_visibilities[0].fill(TreeVisibility::Visible); 
+    tree_visibilities[n-1].fill(TreeVisibility::Visible);
+
+    // for each row, check going from left and right until you cant see past a tree
+    for i in 1..n-1 {
+      // mark always visible
+      tree_visibilities[i][0] = TreeVisibility::Visible;
+      tree_visibilities[i][m-1] = TreeVisibility::Visible;
+
+      let mut tallest = tree_heights[i][0];
+      for j in 1..m-1 {
+        let tree_height = tree_heights[i][j];
+        if tallest < tree_height {
+          tree_visibilities[i][j] = TreeVisibility::Visible;
+          tallest = tree_height;
+        }
+      }
+      let mut tallest = tree_heights[i][m-1];
+      for j in (1..m-1).rev() {
+        let tree_height = tree_heights[i][j];
+        if tallest < tree_height {
+          tree_visibilities[i][j] = TreeVisibility::Visible;
+          tallest = tree_height;
+        }
+      }
+    }
+    // for each col, check going from top and bottom until you cant see past a tree
+    for j in 1..m-1 {
+      let mut tallest = tree_heights[0][j];
+      for i in 1..n-1 {
+        let tree_height = tree_heights[i][j];
+        if tallest < tree_height {
+          tree_visibilities[i][j] = TreeVisibility::Visible;
+          tallest = tree_height;
+        }
+      }
+      let mut tallest = tree_heights[n-1][j];
+      for i in (1..n-1).rev() {
+        let tree_height = tree_heights[i][j];
+        if tallest < tree_height {
+          tree_visibilities[i][j] = TreeVisibility::Visible;
+          tallest = tree_height;
+        }
+      }
+    }
+
+    tree_visibilities
+  }
+}
