558473527f3efeb5046a1c4762b32a70efda34e9
AlekMichael123 <alekmichael2345@gmail.com>
2025-04-26 03:30:03 -0700

    2022 day 13 boilerplate

--- src/aoc/year2022/mod.rs
+++ src/aoc/year2022/mod.rs
@@ -12,6 +12,7 @@ pub mod day9;
 pub mod day10;
 pub mod day11;
 pub mod day12;
+pub mod day13;
 
 pub mod util;
 
@@ -29,6 +30,7 @@ pub fn get_solutions() -> HashMap<u8, fn(&str)> {
     (10, day10::main),
     (11, day11::main),
     (12, day12::main),
+    (13, day13::main),
   ];
 
   solutions.into_iter().collect()
