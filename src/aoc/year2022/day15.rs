b85e5be41f15bd6c2b593ec4b6f4b17e591e4793
AlekMichael123 <alekmichael2345@gmail.com>
2025-05-01 22:01:49 -0700

    2022 day 15 complete, i hated this one

--- src/aoc/year2022/day15.rs
+++ src/aoc/year2022/day15.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashSet, hash::Hash, vec};
+use std::{collections::HashSet, hash::Hash};
 
 pub fn main(data: &str) {
   let sensors = parse_data(data);
@@ -9,12 +9,19 @@ pub fn main(data: &str) {
   let y = 2_000_000;
   let count = part1(&sensors, y);
   println!("Count = {count}");
+
+  println!("Part 2");
+  // let max = 20;
+  let max = 4_000_000;
+  let tuning_frequency = part2(&sensors, max);
+  println!("Tuning frequency = {tuning_frequency}");
 }
 
 fn part1(sensors: &Vec<Sensor>, y: i64) -> i64 {
-
+  // gather all ranges that are covered by sensors on y
   let ranges = Sensor::merge_x_covered_ranges_for_y(y, &mut sensors.clone());
 
+  // find all beacons that are on y to remove them from the count
   let beacons_on_y = sensors
     .iter()
     .filter(|sensor| sensor.beacon.y == y)
@@ -23,12 +30,31 @@ fn part1(sensors: &Vec<Sensor>, y: i64) -> i64 {
     .iter()
     .count();
 
+  // count the number of positions that are covered by sensors on y
   let position_count = ranges
     .iter()
     .map(|range| range.1 - range.0 + 1)
     .sum::<i64>();
   
   position_count - beacons_on_y as i64
+}
+
+// this is a brute force crap solution but i don't care
+fn part2(sensors: &Vec<Sensor>, max: i64) -> i64 {
+  for y in 0..=max {
+    // gather all ranges that are covered by sensors on y
+    let ranges = Sensor::merge_x_covered_ranges_for_y(y, &mut sensors.clone());
+    let mut x = 0;
+    // find the first range that is not covered by any sensor
+    for (_x, _y) in ranges {
+      if _x > x {
+        return x * 4_000_000 + y;
+      } else {
+        x = _y + 1;
+      }
+    }
+  }
+  unreachable!("No solution found")
 }
 
 fn parse_data(data: &str) -> Vec<Sensor> {
@@ -90,11 +116,7 @@ impl Sensor {
 
   // shoutout to for this idea, https://github.com/WinterCore/aoc2022/blob/main/day15/main.rs#L82
   // at first i was just using a HashSet to store the ranges, but that was slow and sucked butt
-  fn merge_x_covered_ranges_for_y(
-    y: i64,
-    ranges: &mut Vec<Sensor>,
-  ) -> Vec<(i64, i64)> {
-    
+  fn merge_x_covered_ranges_for_y(y: i64, ranges: &mut Vec<Sensor>) -> Vec<(i64, i64)> {
     let mut ranges = ranges
       .iter()
       .filter_map(|sensor| sensor.x_covered_range_for_y(y))
