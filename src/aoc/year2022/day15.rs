b9b7abb21d677c680796d040ada94ea97bf85c38
AlekMichael123 <alekmichael2345@gmail.com>
2025-05-01 22:06:01 -0700

    2022 day 15 cleanup

--- src/aoc/year2022/day15.rs
+++ src/aoc/year2022/day15.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashSet, hash::Hash};
+use std::collections::HashSet;
 
 pub fn main(data: &str) {
   let sensors = parse_data(data);
@@ -33,7 +33,7 @@ fn part1(sensors: &Vec<Sensor>, y: i64) -> i64 {
   // count the number of positions that are covered by sensors on y
   let position_count = ranges
     .iter()
-    .map(|range| range.1 - range.0 + 1)
+    .map(|range| range.y - range.x + 1)
     .sum::<i64>();
   
   position_count - beacons_on_y as i64
@@ -46,11 +46,11 @@ fn part2(sensors: &Vec<Sensor>, max: i64) -> i64 {
     let ranges = Sensor::merge_x_covered_ranges_for_y(y, &mut sensors.clone());
     let mut x = 0;
     // find the first range that is not covered by any sensor
-    for (_x, _y) in ranges {
-      if _x > x {
+    for range in ranges {
+      if range.x > x {
         return x * 4_000_000 + y;
       } else {
-        x = _y + 1;
+        x = range.y + 1;
       }
     }
   }
@@ -68,20 +68,20 @@ fn parse_data(data: &str) -> Vec<Sensor> {
     let beacon_y = parts[9].split('=').nth(1).unwrap().parse::<i64>().unwrap();
 
     sensors.push(Sensor::new(
-      Position::new(pos_x, pos_y),
-      Position::new(beacon_x, beacon_y),
+      Range::new(pos_x, pos_y),
+      Range::new(beacon_x, beacon_y),
     ));
   }
   sensors
 }
 
-#[derive(Debug, Clone, Eq, PartialEq, Hash)]
-struct Position {
+#[derive(Debug, Clone, Copy)]
+struct Range {
   x: i64,
   y: i64,
 }
 
-impl Position {
+impl Range {
   fn new(x: i64, y: i64) -> Self {
     Self { x, y }
   }
@@ -89,12 +89,12 @@ impl Position {
 
 #[derive(Debug, Clone)]
 struct Sensor {
-  pos: Position,
-  beacon: Position,
+  pos: Range,
+  beacon: Range,
 }
 
 impl Sensor {
-  fn new(pos: Position, beacon: Position) -> Self {
+  fn new(pos: Range, beacon: Range) -> Self {
     Self { pos, beacon }
   }
 
@@ -102,13 +102,13 @@ impl Sensor {
     (self.pos.x - self.beacon.x).abs() + (self.pos.y - self.beacon.y).abs()
   }
 
-  fn x_covered_range_for_y(&self, y: i64) -> Option<(i64, i64)> {
+  fn x_covered_range_for_y(&self, y: i64) -> Option<Range> {
     let total_range = self.total_range();
     let distance_to_y = (self.pos.y - y).abs();
 
     if distance_to_y <= total_range {
       let x_range = total_range - distance_to_y;
-      Some((self.pos.x - x_range, self.pos.x + x_range))
+      Some(Range::new(self.pos.x - x_range, self.pos.x + x_range))
     } else {
       None
     }
@@ -116,21 +116,21 @@ impl Sensor {
 
   // shoutout to for this idea, https://github.com/WinterCore/aoc2022/blob/main/day15/main.rs#L82
   // at first i was just using a HashSet to store the ranges, but that was slow and sucked butt
-  fn merge_x_covered_ranges_for_y(y: i64, ranges: &mut Vec<Sensor>) -> Vec<(i64, i64)> {
+  fn merge_x_covered_ranges_for_y(y: i64, ranges: &mut Vec<Sensor>) -> Vec<Range> {
     let mut ranges = ranges
       .iter()
       .filter_map(|sensor| sensor.x_covered_range_for_y(y))
-      .collect::<Vec<(i64, i64)>>();
+      .collect::<Vec<Range>>();
 
-    ranges.sort_by_key(|r| r.0);
+    ranges.sort_by_key(|r| r.x);
 
     let mut result = Vec::new();
 
     let mut acc = ranges[0];
     for i in 1..ranges.len() {
       let range = ranges[i];
-      if acc.1 >= (range.0-1) {
-        acc.1 = acc.1.max(range.1);
+      if acc.y >= (range.x-1) {
+        acc.y = acc.y.max(range.y);
       } else {
         result.push(acc);
         acc = range;
@@ -138,7 +138,6 @@ impl Sensor {
     }
 
     result.push(acc);
-
 
     result
   }
